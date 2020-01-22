2ccbc11cb80b11954ac23354cf4a42fb3342a593
antocisale <antonella.cisale@gmail.com>
2020-01-22 12:56:02 -0300

    modificada ubicacion de array de movie info

--- src/App.js
+++ src/App.js
@@ -4,31 +4,11 @@ import './App.css';
 import Card from './Components/Card/Card.js';
 import HeaderImg from './Components/Card/img.jpg';
 import MovieCard from './Components/MovieCard/MovieCard';
-import WonderWomanImg from './Components/MovieCard/wonder_woman.png';
-import LeiaOrganaImg from './Components/MovieCard/Leia_Organa.png';
-import LaraCroftImg from './Components/MovieCard/laraCroft.png';
+import MovieCardInfo from './Components/MovieCard/MovieCardInfo';
 
 const App=() =>{
-  const movieCards=[
-    {
-      title: "Wonder Woman",
-      image:  WonderWomanImg,
-      description: "Diana Prince is the most recognizable female superhero in the world. Her debut film Wonder Womanis the highest grossing superhero origin film of all time. Created in  1941 and beloved for over 76 years, Diana Prince is a strong, compassionate role model for men and women everywhere.",
-      link: "#"
-    },
-    {
-      title: "General Leia Organa",
-      image: LeiaOrganaImg,
-      description: "Leia is the most powerful female figurehead in the Star Wars franchise. This year she will take her final bow in Star Wars: The Last Jedi, as fans celebrate actress Carrie Fisher’s legacy one last time.",
-      link: "#"
-    },
-    {
-      title: "Lara Croft",
-      image: LaraCroftImg,
-      description: "This powerhouse of strength, smarts, and beauty is not to be trifled with. After the recent reboot of her 20-year-old video game franchise Tomb Raider, Lara is headed to the big screen next year for even greater adventures.",
-      link: "#"
-    }
-  ]
+  const movieCards= MovieCardInfo;
+
   return (
     <div className="card-container">
       {movieCards.map(card=>{
@@ -53,7 +33,7 @@ export default App;
     </div>
  */
 
- /**
+/**
   * OPCION 1: PARA MAPEAR IMAGENES Y DUPLICAR CARDS CAMBIANDO EL CONTENIDO
   *  const cards = [
     {
